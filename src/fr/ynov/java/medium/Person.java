b9c1b426ac2bf7c1a7ed3e06ca6c7a717a897b5b
KHMalik03 <khalfamalik2003@gmail.com>
2025-03-10 17:11:24 +0100

    Classes and Enum part 1

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -19,12 +19,7 @@ enum Nationality {
 
 public class Person {
     public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(1990, Calendar.JANUARY, 1);
-        Date birthDate = cal.getTime();
 
-        Person test = new Person("John Doe", "Male", 180.5f, 75.0f, Nationality.AMERICAN, birthDate);
-        test.display();
     }
 
     private final String name;
@@ -32,26 +27,22 @@ public class Person {
     private final float height;
     private final float weight;
     private final Nationality nationality;
-    private final Date birthday;
+    private final int age;
 
-    public Person(String name, String gender, float height, float weight, Nationality nationality, Date birthday) {
+    public Person(String name, String gender, float height, float weight, Nationality nationality, int age) {
         this.name = name;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         this.nationality = nationality;
-        this.birthday = birthday; // Fixed: Use the parameter instead of new Date()
+        this.age = age;
     }
 
-    public int calculateAge() {
-        Date now = new Date();
-        long diff = now.getTime() - birthday.getTime();
-        return (int) (diff / (1000L * 60 * 60 * 24 * 365.25));
-    }
+
 
     public void display() {
         System.out.println("Name: " + name);
-        System.out.println("Age: " + calculateAge());
+        System.out.println("Age: " + age );
         System.out.println("Gender: " + gender);
         System.out.println("Height: " + height + " cm");
         System.out.println("Weight: " + weight + " kg");
